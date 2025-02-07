43e9ccee575d48ab5822f2cdcbeb349a792eef98
KirillSigma <caxap.mel@mail.ru>
2025-02-07 11:21:19 +0300

    qwe

--- tests/test_changing_user.py
+++ tests/test_changing_user.py
@@ -9,8 +9,10 @@ class TestCreateUser:
     def test_changing_authorization_user(self):
         response = User().changing_authorization_user()
         assert response[0] == 200 and response[1]['success'] is True
-        assert response[1]['user'] == {'email': response[2]['email'],
-                               'name': response[2]['name']}
+        assert response[1]['user'] == {
+                                'email': response[2]['email'],
+                                'name': response[2]['name']
+                                }
 
     @allure.title('Тест изменение данных пользователя без авторизацией')
     def test_changing_not_authorization_user(self):
